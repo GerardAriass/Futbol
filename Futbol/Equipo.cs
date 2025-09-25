071bb74d45f0f1d304b9c7ac0ebb126fbb2a8381
Shadrake <abril.goico@gmail.com>
2025-09-25 01:00:34 -0700

    clase equipo

--- Futbol/Equipo.cs
+++ Futbol/Equipo.cs
@@ -8,5 +8,16 @@ namespace Futbol
 {
     internal class Equipo
     {
+        private string _nombre;
+        private int _cantidad;
+
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+
+        public Equipo()
+        {
+            this.Nombre = _nombre;
+            this.Cantidad = _cantidad;
+        }
     }
 }
